66b6c41c971df3e42fbe0544d5cb6bd2515ddd8f
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-12-16 13:13:34 +0600

    added hero section

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Hero = () => {
+    return (
+        <div className='bg-image d-flex flex-column justify-content-center'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h1>Are you hungry!</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores vero maiores amet id perspiciatis? Voluptatum vitae optio, pariatur vero doloribus possimus cumque repellendus sit quam suscipit illum, quos nostrum, ipsum quis eveniet tempore quas quidem dolor tempora assumenda!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from '../../components/Hero/Hero'
 
 const Home = () => {
     return (
         <div>
-            <h2>Homepage</h2>
+            <Hero />
         </div>
     )
 }
